3f090b7dab2d00e23ac059eabd7be55282943198
Nikita S Patil <nikitaspatil311@gmail.com>
2023-07-27 14:21:24 +0200

    vercel usehook fixed

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -86,7 +86,7 @@ const Cart = () => {
           currency: currency,
         },
       });
-    }, []);
+    }, [currency, showSpinner, options, dispatch]);
     //currency, showSpinner
     return (
       <>
