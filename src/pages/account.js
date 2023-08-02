10286e722c43aa2ef5e53b912e7b3014bef084d7
Nikita S Patil <nikitaspatil311@gmail.com>
2023-08-02 09:34:07 +0200

    comment

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,37 +1,37 @@
-import React from "react";
-import { useSession, signOut, getSession } from "next-auth/react";
+// import React from "react";
+// import { useSession, signOut, getSession } from "next-auth/react";
 
-const Account = () => {
-  const { data: session, status } = useSession({ required: true });
-  if (status === "authenticated") {
-    return (
-      <div>
-        <p>Welcome {session.user.name}</p>
-        <button onClick={() => signOut()}>Sign out</button>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <p>You are not Signed in</p>
-      </div>
-    );
-  }
-};
+//  const Account = () => {
+//   const { data: session, status } = useSession({ required: true });
+//   if (status === "authenticated") {
+//     return (
+//       <div>
+//         <p>Welcome {session.user.name}</p>
+//         <button onClick={() => signOut()}>Sign out</button>
+//       </div>
+//     );
+//   } else {
+//     return (
+//       <div>
+//         <p>You are not Signed in</p>
+//       </div>
+//     );
+//   }
+// };
 
-export const getServerSideProps = async (context) => {
-  const session = await getSession(context);
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/login",
-      },
-    };
-  }
+// export const getServerSideProps = async (context) => {
+//   const session = await getSession(context);
+//   if (!session) {
+//     return {
+//       redirect: {
+//         destination: "/login",
+//       },
+//     };
+//   }
 
-  return {
-    props: { session },
-  };
-};
+//   return {
+//     props: { session },
+//   };
+// };
 
-export default Account;
+// export default Account;
