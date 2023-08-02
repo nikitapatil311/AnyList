38ab39b8d07f0a00553f0b0abd41f9f6c1dc332c
Nikita S Patil <nikitaspatil311@gmail.com>
2023-08-02 10:48:00 +0200

    comment6

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  experimental: {
+    forceSwcTransforms: true,
+  },
+
   images: {
     domains: [
       "encrypted-tbn0.gstatic.com",
