8b24f4e9d8142bba08f6570e15f9d3bab839b4d2
Nikita S Patil <nikitaspatil311@gmail.com>
2023-09-09 16:28:24 +0200

    add button

--- components/AddButton.jsx
+++ components/AddButton.jsx
@@ -1,11 +1,11 @@
 import styles from "../styles/Add.module.css";
 
 const AddButton = ({ setClose }) => {
-    return (
-  //     <div onClick={() => setClose(false)} className={styles.mainAddButton}>
-  //       Add New Grocery
-  //     </div>
-    );
+  return (
+    <div onClick={() => setClose(false)} className={styles.mainAddButton}>
+      // Add New Grocery //{" "}
+    </div>
+  );
 };
 
 // export default AddButton;
