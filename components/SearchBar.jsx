51c6021cd8a4d52dfe07d169e8cdfb165c467927
Nikita S Patil <nikitaspatil311@gmail.com>
2023-10-03 12:49:05 +0200

    header css

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "../styles/Search.module.css";
-//import { AiOutlineSearch } from "react-icons/ai";
 
 const SearchBar = ({ searchQuery, setSearchQuery }) => {
   const handleSearchInputChange = (event) => {
@@ -10,9 +9,6 @@ const SearchBar = ({ searchQuery, setSearchQuery }) => {
   return (
     <>
       <div className={styles.searchfull}>
-        {/* <div className={styles.icon}>
-          <AiOutlineSearch className="w-4 text-gray-600" />
-        </div> */}
         <input
           className={styles.container}
           type="text"
