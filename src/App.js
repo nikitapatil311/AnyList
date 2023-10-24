d21ddaedfeddad614d78a2da4a423fcd1541c215
Nikita S Patil <nikitaspatil311@gmail.com>
2023-10-24 11:52:00 +0200

    add changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import React from "react";
+import Home from "./home/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello world!!!!</header>
+      <home />
     </div>
   );
 }
