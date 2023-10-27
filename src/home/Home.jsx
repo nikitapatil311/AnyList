2655ff36d52bfc2b8c26ffa23ac80da575b68652
Nikita S Patil <nikitaspatil311@gmail.com>
2023-10-27 09:38:04 +0200

    add changes

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "../home/home.css";
+import { AcUnit } from "@mui/icons-material";
 
 export default function Home() {
-  return <div className="home">Hello from home</div>;
+  return (
+    <div className="home">
+      Hello from home
+      <AcUnit />
+    </div>
+  );
 }
