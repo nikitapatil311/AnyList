e3b754a94f7ed9d9a85e844469d594bd7899e177
Nikita S Patil <nikitaspatil311@gmail.com>
2023-10-30 15:03:29 +0100

    add changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,7 +4,14 @@ export default function Navbar() {
   return (
     <div className="navbar">
       <div className="container">
-        <div className="left"></div>
+        <div className="left">
+          <img src="" alt="" />
+          <span>Home Page</span>
+          <span>Movies </span>
+          <span>Series </span>
+          <span>New and Popular </span>
+          <span>My List </span>
+        </div>
         <div className="right"></div>
       </div>
     </div>
