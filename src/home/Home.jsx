3c4424f5d5ba4f5b2f623b5bf2afaf2c07626522
Nikita S Patil <nikitaspatil311@gmail.com>
2023-10-30 14:50:03 +0100

    add changes

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../home/home.css";
 import { AcUnit } from "@mui/icons-material";
+import Navbar from "../components/navbar/Navbar";
 
 export default function Home() {
   return (
     <div className="home">
       <AcUnit />
+      <Navbar />
     </div>
   );
 }
