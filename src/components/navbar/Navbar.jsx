bbd2f9acee25efabfea5a96c6ffdaf0feca1f779
Nikita S Patil <nikitaspatil311@gmail.com>
2023-10-30 14:53:05 +0100

    add changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Navbar() {
-  return <div className="navbar">navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="left"></div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
 }
