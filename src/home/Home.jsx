e2ff10237f848df11f82af3dc56b6a7d83b51910
Nikita S Patil <nikitaspatil311@gmail.com>
2023-11-07 16:58:44 +0100

    add scss style

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -21,7 +21,53 @@ export default function Home() {
       ducimus. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
       Aspernatur, nemo voluptatem iure quasi, quidem laborum hic laboriosam
       commodi, eveniet eius modi voluptatibus cupiditate! Sunt vero rerum neque
-      accusamus quis tempora.
+      accusamus quis tempora. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Porro rerum repellendus dolorem perferendis adipisci.
+      Tenetur amet dolor alias explicabo sint libero totam molestiae quis
+      voluptas dolorem quod iusto, non fugit! Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Adipisci, non ea. Aperiam, magnam tempora
+      harum aliquam non totam cumque natus, amet eius voluptate aspernatur
+      culpa! Dicta debitis minima quia ducimus. Lorem ipsum dolor sit amet
+      consectetur, adipisicing elit. Aspernatur, nemo voluptatem iure quasi,
+      quidem laborum hic laboriosam commodi, eveniet eius modi voluptatibus
+      cupiditate! Sunt vero rerum neque accusamus quis tempora. Lorem, ipsum
+      dolor sit amet consectetur adipisicing elit. Porro rerum repellendus
+      dolorem perferendis adipisci. Tenetur amet dolor alias explicabo sint
+      libero totam molestiae quis voluptas dolorem quod iusto, non fugit! Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Adipisci, non ea.
+      Aperiam, magnam tempora harum aliquam non totam cumque natus, amet eius
+      voluptate aspernatur culpa! Dicta debitis minima quia ducimus. Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Aspernatur, nemo voluptatem
+      iure quasi, quidem laborum hic laboriosam commodi, eveniet eius modi
+      voluptatibus cupiditate! Sunt vero rerum neque accusamus quis tempora.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro rerum
+      repellendus dolorem perferendis adipisci. Tenetur amet dolor alias
+      explicabo sint libero totam molestiae quis voluptas dolorem quod iusto,
+      non fugit! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Adipisci, non ea. Aperiam, magnam tempora harum aliquam non totam cumque
+      natus, amet eius voluptate aspernatur culpa! Dicta debitis minima quia
+      ducimus. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Aspernatur, nemo voluptatem iure quasi, quidem laborum hic laboriosam
+      commodi, eveniet eius modi voluptatibus cupiditate! Sunt vero rerum neque
+      accusamus quis tempora. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Porro rerum repellendus dolorem perferendis adipisci.
+      Tenetur amet dolor alias explicabo sint libero totam molestiae quis
+      voluptas dolorem quod iusto, non fugit! Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Adipisci, non ea. Aperiam, magnam tempora
+      harum aliquam non totam cumque natus, amet eius voluptate aspernatur
+      culpa! Dicta debitis minima quia ducimus. Lorem ipsum dolor sit amet
+      consectetur, adipisicing elit. Aspernatur, nemo voluptatem iure quasi,
+      quidem laborum hic laboriosam commodi, eveniet eius modi voluptatibus
+      cupiditate! Sunt vero rerum neque accusamus quis tempora. Lorem, ipsum
+      dolor sit amet consectetur adipisicing elit. Porro rerum repellendus
+      dolorem perferendis adipisci. Tenetur amet dolor alias explicabo sint
+      libero totam molestiae quis voluptas dolorem quod iusto, non fugit! Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Adipisci, non ea.
+      Aperiam, magnam tempora harum aliquam non totam cumque natus, amet eius
+      voluptate aspernatur culpa! Dicta debitis minima quia ducimus. Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Aspernatur, nemo voluptatem
+      iure quasi, quidem laborum hic laboriosam commodi, eveniet eius modi
+      voluptatibus cupiditate! Sunt vero rerum neque accusamus quis tempora.
     </div>
   );
 }
