610388e01c7c2dc501f3dc97c758075016b876b7
Nikita S Patil <nikitaspatil311@gmail.com>
2023-11-07 16:37:05 +0100

    add scss style

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -10,7 +10,10 @@ export default function Home() {
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro rerum
       repellendus dolorem perferendis adipisci. Tenetur amet dolor alias
       explicabo sint libero totam molestiae quis voluptas dolorem quod iusto,
-      non fugit!
+      non fugit! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Adipisci, non ea. Aperiam, magnam tempora harum aliquam non totam cumque
+      natus, amet eius voluptate aspernatur culpa! Dicta debitis minima quia
+      ducimus.
     </div>
   );
 }
