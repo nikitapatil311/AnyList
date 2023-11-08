c2e9ea514095702fcce60c55f98a8de8d9f34e96
Nikita S Patil <nikitaspatil311@gmail.com>
2023-11-08 13:21:29 +0100

    add scss style

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,5 +1,12 @@
 import "../featured/featured.scss";
 
 export default function Featured() {
-  return <div className="featured"></div>;
+  return (
+    <div className="featured">
+      <img
+        src="https://i.pcmag.com/imagery/reviews/05cItXL96l4LE9n02WfDR0h-5.fit_scale.size_760x427.v1582751026.png"
+        alt=""
+      />
+    </div>
+  );
 }
