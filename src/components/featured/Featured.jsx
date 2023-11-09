3e1a2f0203f0e6f7cb979b6540a46f8944ee0c26
Nikita S Patil <nikitaspatil311@gmail.com>
2023-11-09 19:35:20 +0100

    add js changes

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -12,6 +12,7 @@ export default function Featured() {
           src="https://i.pcmag.com/imagery/reviews/05cItXL96l4LE9n02WfDR0h-5.fit_scale.size_760x427.v1582751026.png"
           alt=""
         />
+        <span className="desc"></span>
       </div>
     </div>
   );
