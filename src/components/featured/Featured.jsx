37876e3323a0bf05ef2da28a8aee690b4cc04fa3
Nikita S Patil <nikitaspatil311@gmail.com>
2023-11-21 20:48:08 +0100

    add js changes

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -10,7 +10,7 @@ export default function Featured() {
       />
       <div className="info">
         <img
-          src="https://i.pcmag.com/imagery/reviews/05cItXL96l4LE9n02WfDR0h-5.fit_scale.size_760x427.v1582751026.png"
+          src="https://logos.flamingtext.com/Name-Logos/Benji-design-sketch-name.png"
           alt=""
         />
         <span className="desc">
