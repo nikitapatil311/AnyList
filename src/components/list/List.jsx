39c056e6dbb2862a0e3758eb62c268eeb3070620
Nikita S Patil <nikitaspatil311@gmail.com>
2023-12-01 15:51:22 +0100

    add js changes

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -10,7 +10,7 @@ export default function List() {
     <div className="list">
       <span className="listTitle">Continue to watch</span>
       <div className="wrapper">
-        <ArrowBackIosOutlined className="slideArrow" />
+        <ArrowBackIosOutlined className="slideArrow left" />
         <div className="container">
           <ListItem />
           <ListItem />
