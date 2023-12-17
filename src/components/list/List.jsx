fc488c0c3ad8cb913915760604795a9e06d7dbdb
Nikita S Patil <nikitaspatil311@gmail.com>
2023-12-17 09:16:44 +0100

    add js changes

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -8,9 +8,11 @@ import ListItem from "../listItem/ListItem";
 export default function List() {
   const listRef = useRef();
   const handleClick = (direction) => {
+    let distance = listRef.current.getBoundingClientRect();
     if (direction === "left") {
       listRef.current.style.transform = `translateX(230px)`;
     }
+    console.log(distance);
   };
   return (
     <div className="list">
